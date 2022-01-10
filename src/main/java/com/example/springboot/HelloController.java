55adf365fc7de2f107054d0fc6145db98ff8f52d
Patrick van der Bleek <patrick@bleekie.nl>
2022-01-10 18:57:44 +0100

    Testing

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		return "Happy 2022! Greetings from Spring Boot + Tanzu!";
 	}
 
 }
